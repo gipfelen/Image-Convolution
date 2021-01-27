3cb366e1917e12bb4f13b8f220f8bbd0abe2ebe3
qngapparat <jakob.wallnoefer@outlook.com>
2021-01-27 12:42:46 +0100

    ir-split: get from S3

--- py-functions/ir-split/lambda_function.py
+++ py-functions/ir-split/lambda_function.py
@@ -4,19 +4,22 @@ from moviepy.editor import VideoFileClip
 
 def lambda_handler(event, context):
   numSplits = event['numSplits']
+  s3bucket = event['s3bucket']
   videos3key = event['videos3key']
 
-  s3client = boto3.client('s3')
-  # s3: get
-  clip = VideoFileClip("/tmp/in.mp4")
+  # Get video from S3
+  s3 = boto3.resource('s3')
+  s3.meta.client.download_file(s3bucket, videos3key, '/tmp/video.mp4')
+
+  # Split it into parts, each of length (duration / numSplits)
+  clip = VideoFileClip("/tmp/video.mp4")
   durationSec = clip.duration 
   splitDuration = durationSec / numSplits 
-
   print(durationSec)
   for i in range(numSplits):
     _from = i * splitDuration
     _to = (i+1) * splitDuration 
     print(_from, _to)
     split = clip.subclip(_from, _to)
+    # Save to HDD as 0.mp4, 1.mp4 ...
     split.write_videofile("/tmp" + str(i) + ".mp4")
-
