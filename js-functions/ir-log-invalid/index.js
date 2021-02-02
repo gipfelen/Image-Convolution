7d44189220391557dcbe380d4a16e90da88cec31
qngapparat <jakob.wallnoefer@outlook.com>
2021-02-02 11:53:28 +0100

    init ir-log-invalid w/ trace

---
+++ js-functions/ir-log-invalid/index.js
@@ -0,0 +1,37 @@
+const { pub } = require('./snsutils')
+
+/**
+ * 
+ * @param {{
+ * sampleId: string,
+ * topicArn: string,
+ * region: string,
+ * trace?: string,
+ * sampleTimestamp: Date
+ * }} event 
+ * @param {*} context 
+ * @param {*} callback 
+ */
+exports.handler = async (event, context, callback) => {
+  const sampleId = event['sampleId']
+  const topicArn = event['topicArn']
+  const region = event['region']
+  const trace = event['trace']
+  const sampleTimestamp = event['sampleTimestamp']
+  const loggedTimestamp = new Date()
+  let msg = {
+    sampleId: sampleId,
+    sampleTimestamp: sampleTimestamp,
+    loggedTimestamp: loggedTimestamp,
+    passed: false,
+    trace: trace
+  }
+  msg = JSON.stringify(msg)
+  // Ensure reporting succeeds as it's crucial here
+  try {
+    await pub(msg, topicArn, region)
+    console.log("sent " + msg + " to SNS queue " + topicArn + " " + region)
+  } catch(e) {
+    console.error("Unit did not pass but could not report to Queue.", "Message: " + msg, "Logging Error: " + e)
+  }
+}
