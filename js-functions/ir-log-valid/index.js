82b6aa9c55d8d07eab2a5395843f8cd7792f5ccb
qngapparat <jakob.wallnoefer@outlook.com>
2021-02-02 11:53:14 +0100

    init ir-log-valid function

---
+++ js-functions/ir-log-valid/index.js
@@ -0,0 +1,29 @@
+const { pub } = require('./snsutils')
+
+/**
+ * 
+ * @param {{
+  * sampleId: string,
+  * topicArn: string,
+  * region: string,
+  * sampleTimestamp: Date
+  * }} event 
+  * @param {*} context 
+  * @param {*} callback 
+  */
+exports.handler = async (event, context, callback) => {
+  const sampleId = event['sampleId']
+  const topicArn = event['topicArn']
+  const region = event['region']
+  const sampleTimestamp = event['sampleTimestamp']
+  const loggedTimestamp = new Date()
+  let msg = {
+    sampleId: sampleId,
+    sampleTimestamp: sampleTimestamp,
+    loggedTimestamp: loggedTimestamp,
+    passed: true
+  }
+  msg = JSON.stringify(msg)
+  await pub(msg, topicArn, region)
+  console.log("sent " + msg + " to SNS queue " + topicArn + " " + region)
+}
