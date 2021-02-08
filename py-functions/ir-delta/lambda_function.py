084e1ea29ab5ceed8425b6964d68d5e0cbbee47d
qngapparat <jakob.wallnoefer@outlook.com>
2021-02-08 15:22:44 +0100

    ir-delta: adapt to workflow data flow

--- py-functions/ir-delta/lambda_function.py
+++ py-functions/ir-delta/lambda_function.py
@@ -3,8 +3,9 @@ import datetime
 def lambda_handler(event, context):
 
   cropped_images_s3_keys = event['cropped_images_s3_keys']
+  # in unix epoch millis
   cropped_images_timestamps = event['cropped_images_timestamps']
-  s3bucket = event['s3bucket']
+  #s3bucket = event['s3bucket']
 
   return {
     'all_passed': True,
@@ -14,10 +15,6 @@ def lambda_handler(event, context):
     # When those units were produced
     'invalid_units_timestamps': [],
     # Object storage keys of the image frames ingested
-    'ingested_frame_keys': ['a.jpg','b.jpg'],
-    # The time window for which this function did QA
-    # For instance, if frames were from between Aug 15 00:00:00 - 00:00:15
-    # That would be start_date and end_date
-    'ingested_window_start_date_millis':datetime.datetime.today(), # TODO to millis
-    'ingested_window_end_date_millis':datetime.datetime.today(), # TODO to millis
+    'ingested_frame_keys': cropped_images_s3_keys,
+    'ingested_frame_timestamps': cropped_images_timestamps
   }
