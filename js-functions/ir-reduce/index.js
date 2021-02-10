df9e85807bcbbb79e5d366b1c1ffd5836139bdae
qngapparat <jakob.wallnoefer@outlook.com>
2021-02-10 15:31:32 +0100

    ir-reduce: adapt to data flow

--- js-functions/ir-reduce/index.js
+++ js-functions/ir-reduce/index.js
@@ -1,6 +1,9 @@
-exports.handler = async (event) => {
+exports.handler = async (event, context, callback) => {
   const invalid_units_frame_keys_batches = event['invalid_units_frame_keys_batches']
-  const invalid_units_timestamps_batches = event['invalid_units_timestamps_batches']
+  const invalid_units_frame_timestamps_batches = event['invalid_units_frame_timestamps_batches']
+  const ingested_frame_keys_batches = event['ingested_frame_keys_batches']
+  const ingested_frame_timestamps_batches = event['ingested_frame_timestamps_batches']
+
   
   return {
     'average_deviation': 0.01
