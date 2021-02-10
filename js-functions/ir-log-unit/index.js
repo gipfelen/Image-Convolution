5c40606ec80d174aa7326fd9d63ee7cdc5013cd5
qngapparat <jakob.wallnoefer@outlook.com>
2021-02-10 15:31:07 +0100

    init ir-log-unit

---
+++ js-functions/ir-log-unit/index.js
@@ -0,0 +1,34 @@
+const { pub } = require('./snsutils')
+
+
+exports.handler = async (event, context, callback) => {
+  const all_passed = event['all_passed']
+  const invalid_units_frame_keys = event['invalid_units_frame_keys']
+  const invalid_units_timestamps = event['invalid_units_timestamps']
+  const ingested_frame_keys = event['ingested_frame_keys']
+  const ingested_frame_timestamps = event['ingested_frame_timestamps']
+  const topic_arn = event['topic_arn']
+  const region = event['region']
+    
+  const logged_timestamp = new Date()
+
+  let msg = {
+    all_passed,
+    logged_timestamp
+    invalid_units_frame_keys,
+    invalid_units_timestamps,
+    ingested_frame_keys,
+    ingested_frame_timestamps,
+  }
+
+  msg = JSON.stringify(msg)
+  try {
+    await pub(msg, topicArn, region)
+    console.log("sent " + msg + " to SNS queue " + topicArn + " " + region)
+  } catch(e) {
+    console.error("Unit did not pass but could not report to Queue.", "Message: " + msg, "Logging Error: " + e)
+    throw e
+  }
+
+  return {}
+}
