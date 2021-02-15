e8d94ed4047ae25e7a276c64cde84515a7551231
qngapparat <jakob.wallnoefer@outlook.com>
2021-02-15 14:01:54 +0100

    ir-reduce: implement

--- js-functions/ir-reduce/index.js
+++ js-functions/ir-reduce/index.js
@@ -1,11 +1,12 @@
 exports.handler = async (event, context, callback) => {
-  const invalid_units_frame_keys_batches = event['invalid_units_frame_keys_batches']
-  const invalid_units_frame_timestamps_batches = event['invalid_units_frame_timestamps_batches']
-  const ingested_frame_keys_batches = event['ingested_frame_keys_batches']
-  const ingested_frame_timestamps_batches = event['ingested_frame_timestamps_batches']
+  const invalid_units_frame_keys_batches = event['invalid_units_frame_keys_batches'] || []
+  const invalid_units_frame_timestamps_batches = event['invalid_units_frame_timestamps_batches'] || []
+  const ingested_frame_keys_batches = event['ingested_frame_keys_batches'] || []
+  const ingested_frame_timestamps_batches = event['ingested_frame_timestamps_batches'] || []
 
+  const averageDeviation = invalid_units_frame_keys_batches.length / ingested_frame_keys_batches.length 
   
   return {
-    'average_deviation': 0.01
+    'average_deviation': averageDeviation
   }
 }
